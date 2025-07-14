675c3dca50e94074460a6cae3241a420cbf73928
PabloCalahorro <pablocalahorrojimenez@gmail.com>
2025-07-14 17:42:19 +0200

    (feat) BaseEntity.java added to package com.acainfo.mvp.model

---
+++ src/main/java/com/acainfo/mvp/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.acainfo.mvp.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
